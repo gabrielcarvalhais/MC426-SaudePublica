599aee1508e827dbec74c2ca6d36f96e8d2414c1
Arthur Guerra <conta.arthurguerra@gmail.com>
2023-05-08 18:52:03 -0300

    chore(frontend): adds bootstrap to html pages

--- frontend/js/login/form.js
+++ frontend/js/login/form.js
@@ -1,18 +1,17 @@
-function validate() {
-  var username = document.getElementById("username").value;
+function validate(event) {
+  event.preventDefault()
+  var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
 
-  if (username === "") {
-    document.getElementById("username").style.border = "2px solid red";
-    document.getElementById("username").focus();
-    document.getElementById("error-message").innerHTML = "Please enter your username";
+  if (email === "") {
+    document.getElementById("email").style.border = "2px solid red";
+    document.getElementById("email").focus();
     return false;
   }
 
   if (password === "") {
     document.getElementById("password").style.border = "2px solid red";
     document.getElementById("password").focus();
-    document.getElementById("error-message").innerHTML = "Please enter your password";
     return false;
   }
 
