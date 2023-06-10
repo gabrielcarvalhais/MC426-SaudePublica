c8c5e065262a07241560adc05601d1ee7a809c00
Gabriel Carvalhais <gabrielcarvalhais1@gmail.com>
2023-06-10 19:53:41 -0300

    Entity de agendamento e configuration

---
+++ backend/Domain/Enums/EStatusAgendamento.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MC426_Backend.Domain.Enums
+{
+    public enum EStatusAgendamento
+    {
+        [Display(Name = "Realizado")]
+        Realizado = 1,
+        [Display(Name = "Confirmado")]
+        Confirmado = 2,
+        [Display(Name = "Cancelado")]
+        Cancelado = 3,
+    }
+}
