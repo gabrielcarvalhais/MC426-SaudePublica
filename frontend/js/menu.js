44e30847708fa87003c9b22c370ea1b87eed9900
Gabriel Castilho <gabriel.cama@outlook.com>
2023-06-29 15:00:49 -0300

    Altera classe Menu para gerar menu dinamicamente

--- frontend/js/menu.js
+++ frontend/js/menu.js
@@ -1,5 +1,12 @@
 class Menu {
-  constructor() {
+  constructor(containerId) {
+    this.containerId = containerId;
+  }
 
+  render() {
+    const menuContainer = document.getElementById(this.containerId);
+    addEventListener("load", () => {
+      menuContainer.innerHTML = "<object type='text/html' data='../menu.html' ></object>";
+    });
   }
 }
